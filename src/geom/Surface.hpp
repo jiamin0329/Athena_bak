54493d8515a30f84622a36a855a802e62f279790
Jiamin Xu <jiamin.xu@live.com>
2018-11-18 01:13:34 +0800

    Update libgeom

---
+++ src/geom/Surface.hpp
@@ -0,0 +1,48 @@
+/*
+ *================================================================================
+ * Copyright (c) 2018 Ultra-Aero, All Rights Reserved
+ *================================================================================
+ * File description:
+ * Athena Surface object
+ *
+ *================================================================================
+ *    Date            Name                    Description of Change
+ *    17-Oct-2018     Jiamin Xu               Creation
+ *================================================================================
+ */
+#ifndef ATHENA_SURFACE_HPP
+#define ATHENA_SURFACE_HPP
+
+/* Athena headers */
+
+/* c++ headers */
+
+/*
+ *================================================================================
+ *    Forward declarations
+ *================================================================================
+ */
+namespace ATHENA
+{
+class Point;
+class Curve;
+}  // namespace ATHENA
+
+/*
+ *================================================================================
+ *    Class namespaces
+ *================================================================================
+ */
+namespace ATHENA
+{
+class Surface
+{
+public:
+    Surface();
+    ~Surface(){};
+
+private:
+};
+}  // namespace ATHENA
+
+#endif
