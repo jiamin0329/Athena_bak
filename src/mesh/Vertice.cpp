9ed34c864b3ddfdec7e0a996734f56a2ee2fa543
Jiamin Xu <jiamin.xu@live.com>
2019-04-06 15:56:44 +0800

    Add methods for vertice object

--- src/mesh/Vertice.cpp
+++ src/mesh/Vertice.cpp
@@ -8,6 +8,7 @@
  *================================================================================
  *    Date            Name                    Description of Change
  *    17-Oct-2018     Jiamin Xu               Creation
+ *    07-Apr-2019     Jiamin Xu               Add SetCoord
  *================================================================================
  */
 
@@ -35,5 +36,16 @@ Vertice::Vertice() : d_x(0.0), d_y(0.0), d_z(0.0) {}
 Vertice::Vertice(double x, double y, double z) : d_x(x), d_y(y), d_z(z) {}
 Vertice::Vertice(double x, double y) : d_x(x), d_y(y), d_z(0.0) {}
 Vertice::~Vertice() {}
+
+void
+Vertice::SetCoord(int coord, double value)
+{
+    if (coord == 1)
+        d_x = value;
+    else if (coord == 2)
+        d_y = value;
+    else if (coord == 3)
+        d_z = value;
+}
 
 }  // namespace ATHENA
--- src/mesh/Vertice.hpp
+++ src/mesh/Vertice.hpp
@@ -8,6 +8,7 @@
  *================================================================================
  *    Date            Name                    Description of Change
  *    13-Nov-2018     Jiamin Xu               Creation
+ *    07-Apr-2019     Jiamin Xu               Add SetCoord/GetX/GetY/GetZ
  *================================================================================
  */
 #ifndef ATHENA_VERTICE_HPP
@@ -37,6 +38,12 @@ public:
     Vertice(double x, double y);
     Vertice();
     ~Vertice();
+
+    double GetX() const { return d_x; };
+    double GetY() const { return d_y; };
+    double GetZ() const { return d_z; };
+
+    void SetCoord(int coord, double value);
 
 private:
     int    d_index;
